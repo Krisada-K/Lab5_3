d431509c74dcc1b0683e8734803ec351ccded7ec
Krisada Kareeso <kid-gamer@outlook.com>
2020-09-27 12:44:46 +0000

    new

--- main.c
+++ main.c
@@ -13,23 +13,23 @@ int main(void) {
   scanf("%d",&i);
   switch(i){
   case 1 :
-   printf("%.3f",a+b);
+   printf("Answer is %.3f + %.3f = %.3f",a,b,a+b);
   break;
   case 2 :
-   printf("%.3f",a-b);
+   printf("Answer is %.3f - %.3f = %.3f",a,b,a-b);
   break;
   case 3 :
-   printf("%.3f",a*b);
+   printf("Answer is %.3f * %.3f = %.3f",a,b,a*b);
   break;
   case 4 :
   if(b==0){
-   printf("Devide by ZERO");}
+   printf("Devide by ZERO!!!");}
   else
-  printf("%.3f",a/b);
+  printf("Answer is %.3f / %.3f = %.3f",a,b,a/b);
   break;
   case 5 :
   if(b==0){
-   printf("Devide by ZERO");}
+   printf("Devide by ZERO!!!");}
   else
    printf("Answer is %.3f MOD %.3f = %d",a,b, (int)a%(int)b);
   }
